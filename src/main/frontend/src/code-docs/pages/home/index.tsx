c9b42bf300d3b332d0818e7a1b21e13428740d48
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-02-06 18:41:50 -0300

    Adding more things to home

--- src/main/frontend/src/code-docs/pages/home/index.tsx
+++ src/main/frontend/src/code-docs/pages/home/index.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
 
 import Logo from '../../../code-docs/assets/logo';
+import { Line, Linking, Span } from '../../../core/Styles/utils';
 
-import { Container, TextContent, LogoArea } from './styles';
+import { Container, TextContent, LogoArea, GetStartedArea, ColumnArea } from './styles';
 
 const Home: React.FC = () => {
   return (
-    <Container>
-      <TextContent>
-        <h1>Documenting all of your projects with Docs..</h1>
-        <p>A better way to creating documentation for your projects</p>
-      </TextContent>
-      <LogoArea>
-        <Logo color="#0A72EF" size={465} />
-      </LogoArea>
-    </Container>
+    <React.Fragment>
+      <Container>
+        <TextContent>
+          <h1>Documenting all of your projects with Docs..</h1>
+          <p>A better way to creating documentation for your projects</p>
+          <GetStartedArea>
+            <Linking href='/get-started' className="button-style">
+              <Span>Get Started</Span>
+            </Linking>
+            <Linking href='https://github.com/RubensKj/docs' target='_blank' className="github-button">
+              <Span>GitHub</Span>
+            </Linking>
+          </GetStartedArea>
+        </TextContent>
+        <LogoArea>
+          <Logo color="#0A72EF" size={565} />
+        </LogoArea>
+      </Container>
+      <Line />
+      <ColumnArea>
+        <p>Testing</p>
+      </ColumnArea>
+    </React.Fragment>
   );
 }
 
--- src/main/frontend/src/code-docs/pages/home/styles.ts
+++ src/main/frontend/src/code-docs/pages/home/styles.ts
@@ -2,13 +2,15 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
 
   margin: 0 auto;
   margin-top: 28px;
+
+  max-width: 1328px;
+  width: 100%;
   
   padding-bottom: 78px;
-  border-bottom: 1px solid var(--border-header-color);
 
   @media only screen and (max-width: 1030px) {
     margin-top: 95px;
@@ -21,20 +23,21 @@ export const TextContent = styled.div`
   justify-content: center;
   align-items: center;
 
-  max-width: 465px;
-  margin-right: 45px;
+  max-width: 575px;
+  margin-left: 65px;
 
   h1 {
-    font-size: 48px;
+    font-size: 62px;
     font-weight: 800;
   }
 
   p {
-    font-size: 18px;
+    font-size: 21px;
     font-weight: 400;
     color: rgba(22,30,46,var(--text-opacity));
 
     margin-top: 18px;
+    width: 100%;
   }
 
   @media only screen and (max-width: 1030px) {
@@ -64,4 +67,56 @@ export const LogoArea = styled.div`
   @media only screen and (max-width: 1030px) {
     display: none;
   }
+`;
+
+export const GetStartedArea = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  grid-gap: 12px;
+
+  margin-top: 22px;
+  width: 100%;
+
+  a {
+    border-radius: 4px;
+    border: 0 solid #d2d6dc;
+    padding: 15px 20px;
+
+    span {
+      font-size: 18px;
+      font-weight: 500;
+    }
+
+    transition: 0.2s opacity;
+
+    :hover {
+      opacity: 0.9;
+    }
+  }
+
+  .button-style {
+    color: #FFF;
+    background: #0A72EF;
+    box-shadow: 0 0 2px 3px rgba(66,153,225,0.09);
+  }
+
+  .github-button {
+    color: #0A72EF;
+    background: #FFF;
+    box-shadow: 0px 1px 2px 1px rgba(30,35,90,0.1);
+  }
+`;
+
+export const ColumnArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background: #f9fafb;
+
+  width: 100%;
+  min-height: 400px;
 `;
